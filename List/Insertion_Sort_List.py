bffa9e4d7ca8f2351ec3c80ed6f8f23be55110fd
Li Xiaoman <lixiaoman@Lis-MacBook-Pro-4.local>
2018-07-25 23:36:32 +0800

    refined insertion

--- List/Insertion_Sort_List.py
+++ List/Insertion_Sort_List.py
@@ -12,37 +12,42 @@ class Solution:
     def insertionSortList(self, head):
         newHead = ListNode(0)
         newHead.next = head
-        count = 0
-        while head:
-            count += 1
-            insertPoint = newHead.next
-            count2 = 0
-            while insertPoint and insertPoint.val < head.val:
-                count2 += 1
-                insertPoint = insertPoint.next
-            temp = insertPoint.val
-            insertPoint.val = head.val
-            for i in range(count - count2 - 1):
-                temp2 = insertPoint.next.val
-                insertPoint.next.val = temp
-                temp = temp2
-                insertPoint = insertPoint.next
-            head = head.next
+        while head.next:
+            # insertPoint = newHead
+            # while insertPoint.next and insertPoint.next.val < head.val:
+            #     insertPoint = insertPoint.next
+            # if pre != insertPoint:
+            #     pre.next = head.next
+            #     head.next = insertPoint.next
+            #     insertPoint.next = head
+            #     head = pre.next
+            # else:
+            #     pre = head
+            #     head = head.next
+            if head.next.val < head.val:
+                pre = newHead
+                while pre.next.val < head.next.val:
+                    pre = pre.next
+                temp = head.next
+                head.next = temp.next
+                temp.next = pre.next
+                pre.next = temp
+            else:
+                head = head.next
+
         return newHead.next
 
 
 if __name__ == "__main__":
-    a = ListNode(5)
-    b = ListNode(3)
-    c = ListNode(1)
-    d = ListNode(8)
+    a = ListNode(4)
+    b = ListNode(19)
+    c = ListNode(5)
+    d = ListNode(-1)
     e = ListNode(5)
-    f = ListNode(11)
     a.next = b
     b.next = c
     c.next = d
     d.next = e
-    e.next = f
     reslt = Solution().insertionSortList(a)
     while reslt:
         print((reslt.val))
